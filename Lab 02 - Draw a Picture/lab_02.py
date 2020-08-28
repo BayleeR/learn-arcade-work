a0ce15691ac1e6862dde0e8d197859fce93932f8
Baylee Ratliff <baylee.ratliff@my.simpson.edu>
2020-08-28 17:02:47 -0500

    Started lab 2.

---
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -0,0 +1,14 @@
+import arcade
+
+arcade.open_window(800, 600, "My sample window")
+
+arcade.set_background_color((117, 237, 255))
+
+arcade.start_render()
+
+arcade.draw_lrtb_rectangle_filled(0, 799, 300, 0, arcade.csscolor.GREEN)
+
+arcade.finish_render()
+
+arcade.run()
+
