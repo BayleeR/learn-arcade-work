40d79a14c51968fd4c0dd21862487d49bc8461fe
Baylee Ratliff <baylee.ratliff@my.simpson.edu>
2020-09-03 15:57:45 -0500

    Added work to lab 2.

--- Lab 02 - Draw a Picture/lab_02.py
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -16,17 +16,25 @@ arcade.draw_lrtb_rectangle_filled(0, 800, 225, 200, arcade.color.MEDIUM_VERMILIO
 arcade.draw_circle_filled(550, 200, 80, arcade.color.MELLOW_YELLOW)
 arcade.draw_circle_filled(550, 200, 55, arcade.color.LEMON_CHIFFON)
 
+# Cloud 1
+arcade.draw_ellipse_filled(500, 550, 140, 70, arcade.color.ISABELLINE)
+ #Cloud 2
+arcade.draw_ellipse_filled(650, 500, 100, 50, arcade.color.ISABELLINE)
+
+# Cloud 3
+arcade.draw_ellipse_filled(100, 530, 120, 70, arcade.color.ISABELLINE)
+
 # Bird1
-arcade.draw_arc_outline(600, 350, 35, 30, arcade.color.CHARLESTON_GREEN, 0, 180)
-arcade.draw_arc_outline(635, 350, 35, 30, arcade.color.CHARLESTON_GREEN, 0, 180)
+arcade.draw_arc_outline(600, 350, 35, 30, arcade.color.CHARLESTON_GREEN, 0, 180, 4)
+arcade.draw_arc_outline(635, 350, 35, 30, arcade.color.CHARLESTON_GREEN, 0, 180, 4)
 
 # Bird2
-arcade.draw_arc_outline(550, 410, 30, 25, arcade.color.CHARLESTON_GREEN, 0, 180)
-arcade.draw_arc_outline(580, 410, 30, 25, arcade.color.CHARLESTON_GREEN, 0, 180)
+arcade.draw_arc_outline(550, 410, 30, 25, arcade.color.CHARLESTON_GREEN, 0, 180, 4)
+arcade.draw_arc_outline(580, 410, 30, 25, arcade.color.CHARLESTON_GREEN, 0, 180, 4)
 
 # Bird3
-arcade.draw_arc_outline(525, 310, 15, 10, arcade.color.CHARLESTON_GREEN, 0, 180)
-arcade.draw_arc_outline(540, 310, 15, 10, arcade.color.CHARLESTON_GREEN, 0, 180)
+arcade.draw_arc_outline(525, 310, 15, 10, arcade.color.CHARLESTON_GREEN, 0, 180, 4)
+arcade.draw_arc_outline(540, 310, 15, 10, arcade.color.CHARLESTON_GREEN, 0, 180, 4)
 
 # Grass
 arcade.draw_lrtb_rectangle_filled(0, 800, 200, 0, arcade.color. BITTER_LIME)
@@ -34,8 +42,37 @@ arcade.draw_lrtb_rectangle_filled(0, 800, 200, 0, arcade.color. BITTER_LIME)
 # Concrete
 arcade.draw_lrtb_rectangle_filled(0, 800, 100, 0, arcade.color.DIM_GRAY)
 
+# Sidewalk
+arcade.draw_polygon_filled(((210, 120),
+                            (290, 120),
+                            (320, 100),
+                            (180, 100)
+                            ),
+                           arcade.color.DIM_GRAY)
+
 # House building
+# Building
 arcade.draw_lrtb_rectangle_filled(75, 425, 400, 120, arcade.color.FRENCH_BEIGE)
+# Roof
+arcade.draw_triangle_filled(250, 500, 50, 400, 450, 400, arcade.color.DARK_BROWN)
+# Left window
+arcade.draw_lrtb_rectangle_filled(130, 200, 340, 265, arcade.color.GLITTER)
+arcade.draw_line(165, 340, 165, 265, arcade.color.DIM_GRAY, 4)
+arcade.draw_line(130, 305, 200, 305, arcade.color.DIM_GRAY, 4)
+# Right window
+arcade.draw_lrtb_rectangle_filled(300, 370, 340, 265, arcade.color.GLITTER)
+arcade.draw_line(335, 340, 335, 265, arcade.color.DIM_GRAY, 4)
+arcade.draw_line(300, 305, 370, 305, arcade.color.DIM_GRAY, 4)
+# Door
+arcade.draw_lrtb_rectangle_filled(210, 290, 230, 120, arcade.color.CG_RED)
+# Door knob
+arcade.draw_ellipse_filled(265, 190, 10, 10, arcade.color.KOBE)
+
+# Left bush
+arcade.draw_ellipse_filled(140, 140, 115, 70, arcade.color.JUNE_BUD)
+
+# Right bush
+arcade.draw_ellipse_filled(360, 140, 115, 70, arcade.color.JUNE_BUD)
 
 arcade.finish_render()
 
