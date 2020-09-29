239f522982613763293aafc3528fdc25e887008c
Baylee Ratliff <baylee.ratliff@my.simpson.edu>
2020-09-29 16:28:42 -0500

    Added work to testing and finished lab 5.

--- Testing/test.py
+++ Testing/test.py
@@ -1,17 +1,34 @@
-# Has a
-# These are attributes
-# Person has a name
-# Person has a DOB
-# Person has an account
+""" Sprite Sample Program """
 
-# Is a
-# Employee is a Person
-# Customer is a person
-# Whale is a mammal
+import random
+import arcade
 
-# class Kitchen(room):
+# --- Constants ---
+SPRITE_SCALING_PLAYER = 0.5
+SPRITE_SCALING_COIN = 0.2
+COIN_COUNT = 50
 
-# Not this
-# class Employee(Account):
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
 
+class MyGame(arcade.Window):
+    """ Our custom Window Class"""
+
+    def __init__(self):
+        """ Initializer """
+        # Call the parent class initializer
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprite Example")
+
+    def on_draw(self):
+        arcade.start_render()
+
+
+def main():
+    """ Main method """
+    window = MyGame()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
